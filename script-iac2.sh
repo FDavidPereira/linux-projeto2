644133a96de8b14b31ca1e15c91d8514a4dd6b00
fDavidPereira <david32473247@gmail.com>
2026-09-09 11:29:27 -0400

    apache v.1.0

---
+++ script-iac2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+echo "Atualizando o servgidor.."
+apt-get update
+apt-get install upgrade -y
+apt-get install apache2 -y
+apt-get unzip -y
+
+echo "Baixando e copiando a aplicativo da aplicação..."
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
